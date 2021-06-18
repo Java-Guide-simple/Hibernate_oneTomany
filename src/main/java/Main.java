79580f8282bee0d9f8b6b8de5754c168c4e8ed89
Gautam.Kumar1 <gautam.kumar1@ust.com>
2021-06-18 14:16:55 +0530

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import in.stack.one2many.model.Phone;
+import in.stack.one2many.model.Student;
+import in.stack.one2many.util.HibernateUtil;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+
+			Set<Phone> phoneNumbers = new HashSet<Phone>();
+			phoneNumbers.add(new Phone("house", "32354353"));
+			phoneNumbers.add(new Phone("mobile", "9889343423"));
+
+			Student student = new Student("Eswar", phoneNumbers);
+			session.save(student);
+
+			transaction.commit();
+		} catch (HibernateException e) {
+			transaction.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+
+	}
+
+}
